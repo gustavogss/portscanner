15bf3606e3a74a8774bd8f3d8cc4a4894d24ed45
gustavogss <gustavogss.dev@gmail.com>
2022-11-12 23:47:24 -0300

    Adicionando o IP e a PORTA a ser mapeada

--- pscan.py
+++ pscan.py
@@ -1,1 +1,17 @@
 
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.settimeout(0.05)
+
+IP = input("Digite o host ou ip: ")
+PORT = int(input("Qual é a porta a ser mapeada ? "))
+
+code = client.connect_ex((IP, PORT))
+
+if code == 0:
+    print("Porta Aberta")
+else:
+    print("Porta Fechada")
+
+
